9554cfcc6f0afb6fc16df752de53628171c133cd
DannieMo <danielowiri4zoe@gmail.com>
2023-02-14 16:50:31 +0100

    Added home page render and basic styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 /* eslint-disable */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "capstone project",
-      template: "./src/index.html",
+      title: 'capstone project',
+      template: './src/index.html',
     }),
   ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
