bd0c2bd4270229cd1f72814828573cd7969b92d1
DannieMo <danielowiri4zoe@gmail.com>
2023-02-16 23:23:55 +0100

    Modified index.js and likes.js files and fixed linter errors

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const homeContainer = document.querySelector('.homepage');
 const itemsCountSpan = document.querySelector('.recipe-count');
 const baseUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
 
-const displayMeals = meals => {
+const displayMeals = (meals) => {
   const borderDiv = document.createElement('div');
   borderDiv.className = 'mainCont';
   borderDiv.style.backgroundColor = 'red';
   // eslint-disable-next-line array-callback-return
-  meals.map(meal => {
+  meals.map((meal) => {
     const cardDiv = document.createElement('div');
     const imgDiv = document.createElement('div');
     imgDiv.className = 'meal-photo';
